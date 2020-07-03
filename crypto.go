e3fcb628d78bf5ebb8d555688c07c6be89eda361
Jim McDonald <Jim@mcdee.net>
2020-07-03 17:31:59 +0100

    Update for match spec changes.

--- crypto.go
+++ crypto.go
@@ -34,7 +34,7 @@ func _bigInt(input string) *big.Int {
 var (
 	r = _bigInt("52435875175126190479447740508185965837690552500527637822603658699938581184513")
 	// 48 comes from ceil((1.5 * ceil(log2(r))) / 8)
-	okmSize = 48
+	l = 48
 )
 
 // PrivateKeyFromSeedAndPath generates a private key given a seed and a path.
@@ -90,7 +90,7 @@ func DeriveMasterSK(seed []byte) (*big.Int, error) {
 	if len(seed) < 16 {
 		return nil, errors.New("seed must be at least 128 bits")
 	}
-	return hkdfModR(seed)
+	return hkdfModR(seed, "")
 }
 
 // DeriveChildSK derives the child secret key from a parent key.
@@ -100,34 +100,33 @@ func DeriveChildSK(parentSK *big.Int, index uint32) (*big.Int, error) {
 	if err != nil {
 		return nil, err
 	}
-	return hkdfModR(pk)
+	return hkdfModR(pk, "")
 }
 
 // ikmToLamportSK creates a Lamport secret key.
-func ikmToLamportSK(secret []byte, salt []byte) ([255][32]byte, error) {
-	hash := sha256.New
-	prk := hkdf.Extract(hash, secret, salt)
-	okm := hkdf.Expand(hash, prk, nil)
-	var results [255][32]byte
+func ikmToLamportSK(ikm []byte, salt []byte) ([255][32]byte, error) {
+	prk := hkdf.Extract(sha256.New, ikm, salt)
+	okm := hkdf.Expand(sha256.New, prk, nil)
+	var lamportSK [255][32]byte
 	for i := 0; i < 255; i++ {
 		var result [32]byte
 		read, err := okm.Read(result[:])
 		if err != nil {
-			return results, err
+			return lamportSK, err
 		}
 		if read != 32 {
-			return results, fmt.Errorf("only read %d bytes", read)
+			return lamportSK, fmt.Errorf("only read %d bytes", read)
 		}
-		results[i] = result
+		lamportSK[i] = result
 	}
 
-	return results, nil
+	return lamportSK, nil
 }
 
 // parentSKToLamportPK generates the Lamport private key from a BLS secret key.
 func parentSKToLamportPK(parentSK *big.Int, index uint32) ([]byte, error) {
-	salt := iToOSP(big.NewInt(int64(index)), 4)
-	ikm := iToOSP(parentSK, 32)
+	salt := i2OSP(big.NewInt(int64(index)), 4)
+	ikm := i2OSP(parentSK, 32)
 	lamport0, err := ikmToLamportSK(ikm, salt)
 	if err != nil {
 		return nil, err
@@ -149,20 +148,18 @@ func parentSKToLamportPK(parentSK *big.Int, index uint32) ([]byte, error) {
 }
 
 // hkdfModR hashes 32 random bytes into the subgroup of the BLS12-381 private keys.
-func hkdfModR(ikm []byte) (*big.Int, error) {
-	hash := sha256.New
-	prk := hkdf.Extract(hash, ikm, []byte("BLS-SIG-KEYGEN-SALT-"))
-	okm := hkdf.Expand(hash, prk, nil)
-	okmOut := make([]byte, okmSize)
+func hkdfModR(ikm []byte, keyInfo string) (*big.Int, error) {
+	prk := hkdf.Extract(sha256.New, append(ikm, i2OSP(big.NewInt(0), 1)...), []byte("BLS-SIG-KEYGEN-SALT-"))
+	okm := hkdf.Expand(sha256.New, prk, append([]byte(keyInfo), i2OSP(big.NewInt(int64(l)), 2)...))
+	okmOut := make([]byte, l)
 	read, err := okm.Read(okmOut)
 	if err != nil {
 		return nil, err
 	}
-	if read != okmSize {
+	if read != l {
 		return nil, fmt.Errorf("only read %d bytes", read)
 	}
-	res := osToIP(okmOut)
-	return res.Mod(res, r), nil
+	return new(big.Int).Mod(osToIP(okmOut), r), nil
 }
 
 // osToIP turns a byte array in to an integer as per https://ietf.org/rfc/rfc3447.txt
@@ -170,8 +167,8 @@ func osToIP(data []byte) *big.Int {
 	return new(big.Int).SetBytes(data)
 }
 
-// iToOSP turns an integer in to a byte array as per https://ietf.org/rfc/rfc3447.txt
-func iToOSP(data *big.Int, resLen int) []byte {
+// i2OSP turns an integer in to a byte array as per https://ietf.org/rfc/rfc3447.txt
+func i2OSP(data *big.Int, resLen int) []byte {
 	res := make([]byte, resLen)
 	bytes := data.Bytes()
 	copy(res[resLen-len(bytes):], bytes)
--- go.mod
+++ go.mod
@@ -3,9 +3,11 @@ module github.com/wealdtech/go-eth2-util
 go 1.12
 
 require (
+	github.com/herumi/bls-eth-go-binary v0.0.0-20200703070911-61704dac4ad1 // indirect
 	github.com/pkg/errors v0.9.1
 	github.com/stretchr/testify v1.5.1
 	github.com/wealdtech/go-bytesutil v1.1.1
-	github.com/wealdtech/go-eth2-types/v2 v2.4.2
-	golang.org/x/crypto v0.0.0-20200604202706-70a84ac30bf9
+	github.com/wealdtech/go-eth2-types/v2 v2.4.3
+	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9
+	golang.org/x/sys v0.0.0-20200625212154-ddb9806d33ae // indirect
 )
